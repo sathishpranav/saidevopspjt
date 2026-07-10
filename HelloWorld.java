301b2cd8b182837c4d16f49e7cba860657624c9e
sathishhari <sathishkumar4179@gmail.com>
2026-07-10 17:26:45 +0000

    Updated HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins + Groovy + GitHub SathishArchana!");
-	System.out.println("Hava a Nice day");
+	System.out.println("Hava a Nice day..Sathish Devopsengineer");
     }
 }
+
 
